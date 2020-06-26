b97cfb561ed441daa9cae804a5cbf3d662688649
AyuDiego <48926563+AyuDiego@users.noreply.github.com>
2020-06-26 08:20:13 +0200

    Update validation.js

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,17 +1,19 @@
-
 function validar(){
-    document.getElementById('formId').submit();
-  
-    var todo_correcto = true;
+     
+    // document.getElementById('formulario-js').submit();
+    
+    // var todo_correcto = true;
+
     
     
     
     
-   
+    
     if(!todo_correcto){
-    alert('Algunos campos no están correctos, vuelva a revisarlos');
+      alert('Algunos campos no están correctos, vuelva a revisarlos');
     }
     
     return todo_correcto;
-    }
-  validar();
+  }
+  window.onload = validar();
+   
