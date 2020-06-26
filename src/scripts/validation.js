aa9b94a6a119d8b1156729aecfab6081f99566aa
ddelbara <d.del.barrio.ayuso@accenture.com>
2020-06-26 11:52:53 +0200

    comit

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,19 +1,31 @@
-function validar(){
-     
-    // document.getElementById('formulario-js').submit();
-    
-    // var todo_correcto = true;
+var button = document.getElementById("idBtn");
+button.onclick = function () {
+  validar();
+};
 
+function validar(){
+  
+
+  
+  var name = document.getElementById("name");
+  var subname = document.getElementById("subname");
+  if (name.value.trim() == "" || subname.value.trim()== "" ) {
+    document.getElementById("nameerror").style.display = "block";
+    document.getElementById("nameerror").innerHTML = "Este campo es obligatorio.";
     
+    document.getElementById("subnamerror").style.display = "block";
+    document.getElementById("subnamerror").innerHTML = "Este campo es obligatorio.";
     
-    
-    
-    
-    if(!todo_correcto){
-      alert('Algunos campos no están correctos, vuelva a revisarlos');
-    }
-    
-    return todo_correcto;
+    return false;
+  } else {
+    document.getElementById("nameerror").style.display = "none";
+    document.getElementById("nameerror").innerHTML = "none";
+
+    document.getElementById("subnamerror").style.display = "none";
+    document.getElementById("subnamerror").innerHTML = "none";
+    // document.getElementById("succesuname").style.display = "block";
+    // document.getElementById("succesuname").innerHTML = "Success";
+    true;
   }
-  window.onload = validar();
-   
+  
+}  
